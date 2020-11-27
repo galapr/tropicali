5504ef28db648c63a0a128404eb4533ec73a469a
Galina Priampolsky <gala.prbk@gmail.com>
2020-11-27 16:55:44 +0200

    github deplot script added

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,8 @@ var browserSync = require('browser-sync').create();
 
 var imagemin = require('gulp-imagemin');
 
+var ghpages =  require('gh-pages');
+
 sass.compiler = require('node-sass');
 
 gulp.task("sass", function() {
@@ -55,5 +57,8 @@ gulp.task("watch", function() {
     gulp.watch("src/img/*", ["images"])
 })
 
+gulp.task("deploy", function() {
+    ghpages.publish("dist")
+})
 
 gulp.task('default', ["html", "sass", "fonts", "images", "watch"]);
